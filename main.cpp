016a2e439d232809c3e003cb987462f52a9c046a
Szymongr14 <szymongr14@gmail.com>
2022-11-09 23:47:15 +0100

    first commit plansza i legenda

---
+++ main.cpp
@@ -0,0 +1,166 @@
+#include<stdio.h>
+#include"conio2.h"
+#define BOARD_SIZE 19
+#define DEAFULT_DISPLAY true
+#define BOARD_BORDER_COLOR 13
+
+
+
+
+
+
+void fill_field(char field[][BOARD_SIZE]) {
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		for (int j = 0; j < BOARD_SIZE; j++)
+		{
+			field[j][i] = '*';
+		}
+	}
+}
+
+bool isLegal(int x, int y, int a, int b) {
+	if (y == b) {
+		return false;
+	}
+	return true;
+}
+
+void print_board(int x, int y, char field[][BOARD_SIZE]) {
+	int starty = y;
+	int startx = x;
+	//top border
+	gotoxy(startx -1, starty -1);
+	textcolor(BOARD_BORDER_COLOR);
+	for (int i = 0; i <= (BOARD_SIZE * 2) + 1; i++) {
+		putch('#');
+	}
+	textcolor(WHITE);
+	//printing board
+	for (int i = 0; i < BOARD_SIZE; i++)
+	{
+		x = startx; // y is always ittering from starting value
+		
+		//right border
+		gotoxy(x-1, y);
+		textcolor(BOARD_BORDER_COLOR);
+		putch('#');
+		textcolor(WHITE);
+		for (int j = 0; j < BOARD_SIZE; j++)
+		{
+			gotoxy(x, y);
+			putch(field[i][j]);
+
+			x += 2;
+		}
+		//left border
+		gotoxy(x, y);
+		textcolor(BOARD_BORDER_COLOR);
+		putch('#');
+		textcolor(WHITE);
+		y++;
+	}
+	//bottom border
+	gotoxy(startx - 1, starty + BOARD_SIZE);
+	textcolor(BOARD_BORDER_COLOR);
+	for (int i = 0; i <= (BOARD_SIZE * 2)+1; i++) {
+		putch('#');
+	}
+	textcolor(WHITE);
+}
+
+void print_legend(int startx, int starty, char board[][BOARD_SIZE], int x, int y, int startx_board, int starty_board) {
+	char txt[32];
+	gotoxy(startx, starty++);
+	cputs("     q = exit");
+	gotoxy(startx, starty++);
+	cputs("arrows = moving");
+	gotoxy(startx, starty++);
+	cputs("     i = place a stone");
+	gotoxy(startx, starty++);
+	cputs("     n = new game");
+	sprintf(txt, "current (x,y) : (%d,%d)", x, y);
+	gotoxy(startx, starty++);
+	cputs(txt);
+	gotoxy(startx, 12);
+	//sprintf(txt, "White : (%d,%d)", x, y);
+	//sprintf(txt, "Black : (%d,%d)", x, y);
+	print_board(startx_board, starty_board, board);
+}
+
+int main() {
+	int zn = 0, zero = 0, startx, starty, x, y, legend_x, legend_y;
+	char board[BOARD_SIZE][BOARD_SIZE];
+	char txt[32];
+	bool turnBlack = true;
+	textmode(C80);
+
+#ifndef __cplusplus
+	Conio2_Init();
+#endif
+	settitle("GO GAME");
+	fill_field(board);
+
+	if (DEAFULT_DISPLAY) { // legend-right   board-left
+		
+		startx = 38;
+		//centering board based on screen width and height
+		starty = 13-BOARD_SIZE/2;
+		
+		legend_x = 9;
+		legend_y = 6;
+	}
+	else { // legend-left   board-right
+		startx = 8;
+		starty = 13 - BOARD_SIZE / 2;
+		legend_x = 50;
+		legend_y = 7;
+	}
+	x = startx;
+	y = starty;
+
+	_setcursortype(_NOCURSOR);
+	do {
+		
+		
+		clrscr();
+		gotoxy(25, 1);
+		cputs("Szymon Groszkowski nr 193141");
+		
+		
+		print_legend(legend_x, legend_y, board, x, y, startx, starty);
+		
+		
+		gotoxy(x, y);
+		putch('@');
+
+		
+		zero = 0;
+		zn = getch();
+
+		//cursor moving only in board area
+		if(zn == 0) {
+			zero = 1;		
+			zn = getch();		
+			if (zn == 0x48 && y!= starty) y--;
+			else if (zn == 0x50 && y!=starty+(BOARD_SIZE-1)) y++;
+			else if(zn == 0x4b && x!=startx) x-=2;
+			else if(zn == 0x4d && x != startx+(BOARD_SIZE*2-2)) x+=2;
+		}
+		// putting stone
+		if (zn == 'i') {
+			if (turnBlack) {
+				board[y - starty][(x - startx) / 2] = 'X';
+				turnBlack = false;
+			}
+			else {
+				board[y - starty][(x - startx) / 2] = 'O';
+				turnBlack = true;
+			}
+		}
+	} while (zn != 'q');
+
+	_setcursortype(_NORMALCURSOR);
+					
+	return 0;
+	}
